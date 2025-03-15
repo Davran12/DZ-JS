a11b983ee88644da857d4ce881d4d9b9ff8bf268
Davran <davranlapa@gmail.com>
2025-03-15 18:56:13 +0600

    DZ-JS

---
+++ main.js
@@ -0,0 +1,26 @@
+// let num = "Привет Мир"
+// console.log(num)
+
+// let a = 10
+// let b = 2
+// let c = 5
+// console.log(a + b + c)
+
+// let num1 = 100
+// console.log(num1 * 2)
+
+// let a = 10
+// let b = 2
+// console.log(a + b, a - b, a / b, a * b)
+
+// let c = 15
+// let d = 2
+// let result = c + d
+// console.log(result)
+
+// let a = 17
+// let b = 10
+// let c = a - b
+// let d = 15
+// let result = c + d
+// console.log(result)
