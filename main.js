98fed961ed98e41228b52e28ef5690d68ff5f62b
Davran <davranlapa@gmail.com>
2025-03-20 19:37:26 +0600

    update

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-// let num = "Привет Мир"
-// console.log(num)
+let text = "  JavaScript — это мощный язык программирования!  "
 
-// let a = 10
-// let b = 2
-// let c = 5
-// console.log(a + b + c)
+// 1. Удалите лишние пробелы в начале и конце строки
+let trimmedText = text.trim()
 
-// let num1 = 100
-// console.log(num1 * 2)
+// 2. Замените слово "мощный" на "гибкий"
+let replacedText = text.replace("мощный", "гибкий")
 
-// let a = 10
-// let b = 2
-// console.log(a + b, a - b, a / b, a * b)
+// 3. Выведите первый и последний символ строки
+let firstChar = text.slice(0, 12)
+let lastChar = text.slice(30, 48)
 
-// let c = 15
-// let d = 2
-// let result = c + d
-// console.log(result)
+// 4. Разделите строку на массив слов
+let wordsArray = text.split()
 
-// let a = 17
-// let b = 10
-// let c = a - b
-// let d = 15
-// let result = c + d
-// console.log(result)
+// 5. Преобразуйте всю строку в верхний регистр
+let upperCaseText = text.toUpperCase()
+
+// 6. Вырежьте слово "JavaScript" из строки
+let slicedText = text.slice(0, 12)
+
+// 7. Найдите длину исходной строки
+let textLength = text.length
+
+// 8. Соедините две строки: исходную (без пробелов) и " Учите его каждый день!"
+let finalText = trimmedText + " Учите его каждый день!"
+// Вывод результатов
+console.log(trimmedText)
+console.log(replacedText)
+console.log(firstChar)
+console.log(lastChar)
+console.log(wordsArray)
+console.log(upperCaseText)
+console.log(slicedText)
+console.log(textLength)
+console.log(finalText)
