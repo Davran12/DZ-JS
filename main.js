6e6213cd4aef6dd835bccac51c474df98a9a88c3
Davran <davranlapa@gmail.com>
2025-03-26 18:35:13 +0600

    dz

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
-// console.log(5 > 3)
-// console.log(10 == 10)
-// console.log(15 < 10)
-// console.log(20 >= 15)
+// ! Задание 1
+// const phone = "+7 (550) 225-456"
+// let phoneCountry
+// if (phone.startsWith("+996")) {
+//   phoneCountry = "KG"
+// } else if (phone.startsWith("+7")) {
+//   phoneCountry = "KZ"
+// } else if (phone.startsWith("+1")) {
+//   phoneCountry = "US"
+// }
+// console.log(phoneCountry)
+// ! Задание 2
+// let year = 1995
+// let isAlmostLeapYear
 
-let money = 500
-let price = 150
-// console.log(money >= price)
+// if (year % 4 === 0 && year % 100 !== 0 && (year + 1) % 4 === 0) {
+//   isAlmostLeapYear = true
+// } else {
+//   isAlmostLeapYear = false
+// }
 
-let isCorrectLogin = true
-let isCorrectPassword = false
-let isAdmin = true
-console.log((isCorrectLogin && isCorrectPassword) || isAdmin)
+// console.log(isAlmostLeapYear)
+
+// ! Задание 3
+
+// let num = 35
+// let result
+
+// switch (true) {
+//   case num % 7 === 0 && num % 5 === 0:
+//     result = "Семь и пять!"
+//     break
+//   case num % 7 === 0:
+//     result = "Семёрка!"
+//     break
+//   case num % 5 === 0:
+//     result = "Пятёрка!"
+//     break
+//   default:
+//     result = "Обычное число"
+// }
+
+// console.log(result)
