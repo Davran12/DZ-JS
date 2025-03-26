6a692f5dd0d3f183eab22526c53ca42081676962
Davran <davranlapa@gmail.com>
2025-03-26 22:36:01 +0600

    first commit

--- main.js
+++ main.js
@@ -1,43 +1,14 @@
-// ! Задание 1
-// const phone = "+7 (550) 225-456"
-// let phoneCountry
-// if (phone.startsWith("+996")) {
-//   phoneCountry = "KG"
-// } else if (phone.startsWith("+7")) {
-//   phoneCountry = "KZ"
-// } else if (phone.startsWith("+1")) {
-//   phoneCountry = "US"
-// }
-// console.log(phoneCountry)
-// ! Задание 2
-// let year = 1995
-// let isAlmostLeapYear
+let a = [1, {test2: {true: false}, test: 21}, {meta: ["metalabs", 113]}]
 
-// if (year % 4 === 0 && year % 100 !== 0 && (year + 1) % 4 === 0) {
-//   isAlmostLeapYear = true
-// } else {
-//   isAlmostLeapYear = false
-// }
+let arr = [
+  "color",
+  [
+    1222,
+    {
+      meta: ["labs", 117673],
+      meta: ["labs", 110, {finally: "check", arr2: ["hello world", a]}],
+    },
+  ],
+]
 
-// console.log(isAlmostLeapYear)
-
-// ! Задание 3
-
-// let num = 35
-// let result
-
-// switch (true) {
-//   case num % 7 === 0 && num % 5 === 0:
-//     result = "Семь и пять!"
-//     break
-//   case num % 7 === 0:
-//     result = "Семёрка!"
-//     break
-//   case num % 5 === 0:
-//     result = "Пятёрка!"
-//     break
-//   default:
-//     result = "Обычное число"
-// }
-
-// console.log(result)
+console.log(arr[1][1].meta[2].arr2[1][2].meta[0])
