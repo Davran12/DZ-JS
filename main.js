2dd1a025960271152a8cd7588269763bde26ee26
Davran <davranlapa@gmail.com>
2025-05-01 15:50:03 +0600

    dz42

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-function checkDivisibility(dividend, ...divisors) {
-  const result = {
-    divisible: [],
-    notDivisible: [],
-  }
+// const celsiusTemperatures = [0, 25, 30, -5, 15]
 
-  for (let i = 0; i < divisors.length; i++) {
-    if (dividend % divisors[i] === 0) {
-      result.divisible.push(divisors[i])
-    } else {
-      result.notDivisible.push(divisors[i])
-    }
-  }
+// function convert(arr) {
+//   return arr.map((temp) => (temp * 9) / 5 + 32)
+// }
 
-  return result
-}
-console.log(checkDivisibility(15, 3, 5, 4, 7))
+// const result = convert(celsiusTemperatures)
+// console.log(result)
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const result = numbers.filter((num) => num % 3 === 0 && num % 9 === 0)
+
+// console.log(result)
