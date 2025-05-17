d86144e3cd3bb3f085ad3c609c1862e8464a23cb
Davran <davranlapa@gmail.com>
2025-05-17 22:24:29 +0600

    dz

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-function findMinValueInObjects(objects) {
-  return Math.min(...objects.map((obj) => obj.value))
-}
-function splitEvenOdd(numbers) {
-  const result = {even: [], odd: []}
-  for (const num of numbers) {
-    if (num % 2 === 0) {
-      result.even.push(num)
-    } else {
-      result.odd.push(num)
-    }
+const listItems = document.querySelectorAll("#numbers-list li")
+listItems.forEach((li, index) => {
+  if ((index + 1) % 2 === 0) {
+    li.style.color = "red"
+  } else {
+    li.style.color = "blue"
   }
-  return result
-}
-const objects7 = [{value: 10}, {value: 5}, {value: 20}]
-console.log("7. Минимальное значение:", findMinValueInObjects(objects7)) // 5
 
-const numbers8 = [1, 2, 3, 4, 5]
-console.log("8. Чётные и нечётные:", splitEvenOdd(numbers8))
+  const divs = document.querySelectorAll("div")
+  divs.forEach((div) => {
+    if (div.textContent.trim() === "Hello") {
+      console.log(div)
+    }
+  })
+})
