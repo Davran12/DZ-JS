f7003a4f9aef5fc1ab2b38ec1bc9573e827077e1
Davran <davranlapa@gmail.com>
2025-07-04 22:39:10 +0600

    dz66

--- main.js
+++ main.js
@@ -1,36 +1,72 @@
-// function reverseNumber(n, result = 0) {
-//   if (n === 0) return result
-//   return reverseNumber(Math.floor(n / 10), result * 10 + (n % 10))
-// }
+async function sequencePromises(funcs) {
+  const results = []
 
-// console.log(reverseNumber(123))
-// console.log(reverseNumber(1000))
-// console.log(reverseNumber(5))
-
-// ! Задание #2
-
-function countKeys(obj) {
-  let count = 0
-  for (let key in obj) {
-    count++
-    if (typeof obj[key] === "object" && obj[key] !== null) {
-      count += countKeys(obj[key])
+  try {
+    for (const fn of funcs) {
+      const result = await fn()
+      results.push(result)
     }
+    return results
+  } catch (e) {
+    return null
   }
-  return count
 }
 
-const nestedObj = {
-  a: 1,
-  b: {
-    c: 2,
-    d: {
-      e: 3,
-    },
-  },
-  f: {
-    g: 4,
-  },
+const promise1 = () => Promise.resolve(1)
+const promise2 = () => Promise.resolve(2)
+const promise3 = () => Promise.reject("Error")
+
+sequencePromises([promise1, promise2]).then(console.log) // [1, 2]
+
+sequencePromises([promise1, promise3, promise2]).then(console.log) // null
+
+// --------------------------------------------------------
+
+async function fetchUserPosts(userId) {
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+    )
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok")
+    }
+
+    const posts = await response.json()
+    return posts
+  } catch (error) {
+    return "Failed to fetch posts"
+  }
 }
 
-console.log(countKeys(nestedObj))
+fetchUserPosts(1).then(console.log)
+fetchUserPosts(9999).then(console.log)
+
+async function createNewPost(postData) {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error("Request failed")
+    }
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    return {error: "Failed to create post"}
+  }
+}
+
+const newPost = {
+  title: "New Post",
+  body: "This is a new post",
+  userId: 1,
+}
+
+createNewPost(newPost).then(console.log).catch(console.error)
